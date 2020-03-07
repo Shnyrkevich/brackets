827ae981b3071088cc29c22bcb032e53a13ee2af
Евгений Шныркевич <mogrein.dark@gmail.com>
2020-03-07 22:26:32 +0300

    feat: realize brackets function with gods help

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let len = str.length;
+  let counter = 0;
+  let mas = [];
+
+  for(let i = 0; i <  bracketsConfig.length;){
+    mas[i] = bracketsConfig[i].join(""); 
+    
+    if(str.includes(mas[i])){
+      str = str.replace(mas[i], "");
+      counter++;
+      i=0;
+    } else {
+      i++;
+    }
+  
+    console.log("String:", str);
+  } 
+  console.log("len", len/2, counter);
+
+  if(len/2 == counter) {
+    return true;
+  } else {
+    return false;
+  }
+
+ // console.log( str.includes(bracketsConfig[0].join('')));
+
 }
